b625a07d502ebda2ecfc60bccd9bea354b5b8447
AVJDUC <a.valdiviaj@duocuc.cl>
2021-07-09 18:11:04 -0400

    conex bd y modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    idCliente = models.IntegerField(primary_key=True, verbose_name="Id Categoria")
+    suscripcion = models.BooleanField(default=False, verbose_name="Suscripcion")
+
+    def _str_(self):
+        return self.idCliente
+
+# modelo usuario
+
+class Usuario(models.Model):
+    idUsuario = models.IntegerField(primary_key=True, verbose_name="Id Usuario")
+    username = models.CharField(max_length=20, null=True, blank=True, verbose_name="nombre usuario")
+    password = models.CharField(max_length=20, null=True, blank=True, verbose_name="Modelo")
+    idCliente= models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.idUsuario
+
+# modelo producto
+
+class Producto(models.Model):
+    idProducto = models.IntegerField(primary_key=True, verbose_name="Id Usuario")
+    nProducto = models.CharField(max_length=30, null=True, blank=True, verbose_name="nombre producto")
+    cantidad = models.IntegerField(max_length=10, null=True, blank=True, verbose_name="cantidad producto")
+    descripcion = models.CharField(max_length=20, null=True, blank=True, verbose_name="descripcion producto")
+    idVenta= models.ForeignKey(Venta, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.idProducto
+
+class Venta(models.Model):
+    idVenta = models.IntegerField(primary_key=True, verbose_name="Id Usuario")
+    idCliente= models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.idVenta
+
